a443feefec56f0a9baf53b83cab43fae7d1b2a8d
Alexander Penzov <penzov.ids@gmail.com>
2015-07-07 00:27:36 +0300

    enable cross-domain query

--- app.js
+++ app.js
@@ -48,6 +48,12 @@ passport.use(new BearerStrategy({
     }
 ));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/', routes);
 app.use('/users', /*passport.authenticate('bearer', { session: false }),*/ users);
 
